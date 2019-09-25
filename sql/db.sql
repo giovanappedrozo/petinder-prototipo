0a0d0015b71671df4baa36664bebe4ec5cf45ec2
Brenda <breendasouusaa@gmail.com>
2019-09-25 13:05:06 -0300

    Populando as tabelas

--- sql/db.sql
+++ sql/db.sql
@@ -1,4 +1,3 @@
-
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
@@ -17,6 +16,18 @@ CREATE TABLE IF NOT EXISTS `petinder`.`USUARIO` (
   PRIMARY KEY (`cod`))
 ENGINE = InnoDB;
 
+         INSERT INTO USUARIO values ('Clovis', 'clovis@gmail.com', 'cloviss0022', 'cachorro', 'apartamento', 'procurando cachorro de porte pequeno')
+         INSERT INTO USUARIO values ('Marcia', 'marcia88maria@gmail.com', 'alo22haha', 'cachorro', 'apartamento', 'procurando cachorro de porte medio')
+         INSERT INTO USUARIO values ('Bruna','brunahsp@hotmail.com', 'rubs55castroa', 'gato', 'casa', 'procurando gato carinhoso')
+         INSERT INTO USUARIO values ('Eduardo', 'eduardopickius@gmail.com', '58472961', 'cachorro', 'apartamneto', 'procurando cachorro pug')
+         INSERT INTO USUARIO values ('Carolinna', 'carolzanatta77@yahoo.com', 'beldincarol96', 'cachorro', 'casa', 'animal porte pequeno')
+         INSERT INTO USUARIO values ('Luciana', 'lucianaandonini@hotmail.com', 'lulunini@ha', 'gato', 'casa', 'so quero um gato')
+         INSERT INTO USUARIO values ('Mariana', 'tyethomaz@gmail.com', 'theytheymari','gato','casa', 'procurando gato raça persa')
+         INSERT INTO USUARIO values ('Guilherme','gui9007@outlook.com', 'guiAraujo963', 'cachorro', 'apartamento', 'cachorro de porte pequeno de preferência')
+         INSERT INTO USUARIO values ('André','andre.b.feistauer@gmail.com', 'barrosfeeis', 'cachorro','apartamento', ' cachorro de porte grande')
+         INSERT INTO USUARIO values ('Larissa', 'larissaambonatti@hotmail.com', 'bonattilala', 'gato','casa', 'oi, quero gatos')
+
+
 CREATE TABLE IF NOT EXISTS `petinder`.`ADMIN` (
   `cod` INT NOT NULL AUTO_INCREMENT,
   `qt_animais` INT NOT NULL,
@@ -25,6 +36,17 @@ CREATE TABLE IF NOT EXISTS `petinder`.`ADMIN` (
   `senha` CHAR(10) NOT NULL,
   PRIMARY KEY (`cod`))
 ENGINE = InnoDB;
+INSERT INTO ADMIN VALUES 
+(2, 'Lucas', 'lucas303@gmail.com', '0101010101'),
+(0, 'Fábio', 'ffbio@gmail.com', '0705030200'),
+(1, 'Gabriela', 'gabiiii@gmail.com', '1654851548'),
+(0, 'Saulo', 'saulosaulo@gmail.com', '4518465355'),
+(2, 'Pedro', 'pedrinho01@gmail.com', 'guyhsf6158'),
+(1, 'Vanessa', 'vansan@gmail.com', '48545jnfbs'),
+(1, 'Rodrigo', 'roddiaz@gmail.com', 'jnfaiufh33'),
+(3, 'Thamires', 'thata@gmail.com', '595715jgff'),
+(0, 'Carla', 'carlinha00@gmail.com', '615484hj00'),
+(0, 'Giovana', 'giovanaaa@gmail.com', 'uyahcu2058'),
 
 CREATE TABLE IF NOT EXISTS `petinder`.`MENSAGEM` (
   `cod` INT NOT NULL AUTO_INCREMENT,
@@ -46,11 +68,39 @@ CREATE TABLE IF NOT EXISTS `petinder`.`MENSAGEM` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
+INSERT INTO MENSAGEM VALUES
+(DATETIME, 'hvjtghjbktfkyuhjbvgf', 1, 4)
+(DATETIME, 'jbuhjliandbyukkhdus', 3, 1)
+(DATETIME, 'jshb fjdgbduy', 7, 2)
+(DATETIME, 'mikjniuhii', 9, 1)
+(DATETIME, 'psofnhefnjaiufheby n', 4, 7)
+(DATETIME, 'knsxuvyh jhbfkys', 3, 9)
+(DATETIME, 'oawijdaiuwh898r5893', 10, 3)
+(DATETIME, 'dijvgrdiuf', 9, 2)
+(DATETIME, 'apojdiujwiudhqiwu', 1, 10)
+(DATETIME, 'ijvksxnfvjkbn iuihnugh jhuy', 5, 2)
 
 CREATE TABLE IF NOT EXISTS `petinder`.`ESPECIE` (
   `cod` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`cod`))
+ENGINE = InnoDB;
+
+INSERT INTO ESPECIE  values('Gato', '1', 'Felina ')
+INSERT INTO ESPECIE  values('Cachorro', '2', 'Canina ')
+
+
+CREATE TABLE IF NOT EXISTS `petinder`.`RACA` (
+  `cod` INT NOT NULL,
+  `nome` VARCHAR(45) NOT NULL,
+  `ESPECIE_cod` INT NOT NULL,
+  PRIMARY KEY (`cod`),
+  INDEX `fk_RACA_ESPECIE1_idx` (`ESPECIE_cod` ASC),
+  CONSTRAINT `fk_RACA_ESPECIE1`
+    FOREIGN KEY (`ESPECIE_cod`)
+    REFERENCES `petinder`.`ESPECIE` (`cod`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `petinder`.`ANIMAL` (
@@ -75,7 +125,23 @@ CREATE TABLE IF NOT EXISTS `petinder`.`ANIMAL` (
     REFERENCES `petinder`.`ESPECIE` (`cod`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
+    CONSTRAINT `fk_ANIMAL_RACA1`
+    FOREIGN KEY (`RACA_cod`)
+    REFERENCES `petinder`.`RACA` (`cod`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
+INSERT INTO ANIMAL  values('medio' , 'F' , '02', 'Animal abandonado ao lado de um rio', 'Zury', '1','01') 
+INSERT INTO ANIMAL  values('pequeno' , 'F' , '13', 'Antiga cadela com fins de procriação', 'Lulu', '2', '02') 
+INSERT INTO ANIMAL  values('grande' , 'M' , '08', 'Cachorro adestrado e carinhoso', 'Donnie', '3', '02') 
+INSERT INTO ANIMAL  values('pequeno' , 'F' , '06', 'Gata arrisca', 'Safira', '4','01') 
+INSERT INTO ANIMAL  values('medio' , 'M' , '07', 'Gato castrado e amoroso', 'Saulo', '5', '01') 
+INSERT INTO ANIMAL  values('grande' , 'M' , '09', 'Cachorro decorrente do cruzamento de 2 espécies muito caras', 'Dondoca', '6', '02') 
+INSERT INTO ANIMAL  values('grande' , 'F' , '15', 'Cadela idosa que foi abandonada no meio de uma rodovia', 'Neide', '7','02') 
+INSERT INTO ANIMAL  values('pequeno' , 'F' , '16', 'Gata idosa que precisa de uma vida calma e tem muito amor para dar', 'Fluflu', '8', '01') 
+INSERT INTO ANIMAL  values('grande' , 'F' , '03', 'Gata castrada e energetica', 'Guta', '9', '01') 
+INSERT INTO ANIMAL  values('pequeno' , 'M' , '04', 'Cachorro filhote que aprende rápido e é muito atencioso', 'Scooby', '10', '02') 
+
 
 CREATE TABLE IF NOT EXISTS `petinder`.`IMG_ANIMAL` (
   `cod` INT NOT NULL AUTO_INCREMENT,
@@ -90,19 +156,16 @@ CREATE TABLE IF NOT EXISTS `petinder`.`IMG_ANIMAL` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `petinder`.`RACA` (
-  `cod` INT NOT NULL,
-  `nome` VARCHAR(45) NOT NULL,
-  `ESPECIE_cod` INT NOT NULL,
-  PRIMARY KEY (`cod`),
-  INDEX `fk_RACA_ESPECIE1_idx` (`ESPECIE_cod` ASC),
-  CONSTRAINT `fk_RACA_ESPECIE1`
-    FOREIGN KEY (`ESPECIE_cod`)
-    REFERENCES `petinder`.`ESPECIE` (`cod`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
+INSERT INTO tb_imgAnimal  values ('C:\Users\Public\Pictures\Sample Pictures', 1)
+INSERT INTO tb_imgAnimal  values ('C:\Users\Public\Pictures\Sample Pictures', 2)
+INSERT INTO tb_imgAnimal  values ('C:\Users\Public\Pictures\Sample Pictures', 3)
+INSERT INTO tb_imgAnimal  values ('C:\Users\Public\Pictures\Sample Pictures', 4)
+INSERT INTO tb_imgAnimal  values ('C:\Users\Public\Pictures\Sample Pictures', 5)
+INSERT INTO tb_imgAnimal  values ('C:\Users\Public\Pictures\Sample Pictures', 6)
+INSERT INTO tb_imgAnimal  values ('C:\Users\Public\Pictures\Sample Pictures', 7)
+INSERT INTO tb_imgAnimal  values ('C:\Users\Public\Pictures\Sample Pictures', 8)
+INSERT INTO tb_imgAnimal  values ('C:\Users\Public\Pictures\Sample Pictures', 9)
+INSERT INTO tb_imgAnimal  values ('C:\Users\Public\Pictures\Sample Pictures', 10)
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
